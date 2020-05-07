80177f13c167737cdbcaf354a7a4adafb46fee3b
Wen <l1048445160@live.com>
2020-05-07 14:57:38 +0800

    docs(TradingAppCrawler/connector): update docs.

--- TradingAppCrawler/connector.py
+++ TradingAppCrawler/connector.py
@@ -4,10 +4,17 @@ import uiautomator2
 
 
 class Connector:
+    """
+    负责连接不同的设备
+    """
     def __init__(self, platform, address):
         self.platform = platform
         self.address = address
 
     def connect(self):
+        """
+        开始连接，返回连接的设备对象
+        :return: Device
+        """
         if self.platform == 'android':
             return uiautomator2.connect(self.address)
