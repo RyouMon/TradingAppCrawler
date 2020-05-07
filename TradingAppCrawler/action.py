0ee49a0e51ecca6f1a156bd5271594b07ed00559
Wen <l1048445160@live.com>
2020-05-07 14:38:00 +0800

    feat(TradingAppCrawler): new module action.py.

---
+++ TradingAppCrawler/action.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Action:
+    def __init__(self, device):
+        self.device = device
+
+    def open_app(self, package_name):
+        pass
+
+    def refresh(self):
+        pass
+
+    def back(self):
+        pass
+
+    def click(self):
+        pass
