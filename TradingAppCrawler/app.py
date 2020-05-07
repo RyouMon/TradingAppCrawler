d5373855ded28f9fa19f35d74181da40ee79be38
Wen <l1048445160@live.com>
2020-05-07 14:46:13 +0800

    feat(TradingAppCrawler): new module app.py.

---
+++ TradingAppCrawler/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+保存app中元素的 course id
+"""
+import types
+
+
+du = types.SimpleNamespace(
+    user_btn='com.shizhuang.duapp:id/rbtn_user',
+    seller_center='com.shizhuang.duapp:id/sellerInfoUp',
+    trade_success_num='com.shizhuang.duapp:id/tvTradeSuccessNum',
+    trade_failed_num='com.shizhuang.duapp:id/tvTradeFailedNum',
+)
