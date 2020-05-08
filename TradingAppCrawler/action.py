31e6c952dda9e096a3415934673426b1a9592957
Wen <l1048445160@live.com>
2020-05-08 18:14:54 +0800

    fix(TradingAppCrawler/action.py): new class AndroidAction, new function create_action().

--- TradingAppCrawler/action.py
+++ TradingAppCrawler/action.py
@@ -4,7 +4,7 @@
 
 class Action:
     """
-    完成对设备的基本操作
+    Base Action.
     """
     def __init__(self, device):
         self.device = device
@@ -20,3 +20,48 @@ class Action:
 
     def click(self):
         pass
+
+    def click_by_resource_id(self, resource_id):
+        pass
+
+
+class AndroidAction(Action):
+    """
+    Base actions for Android
+    """
+    def click_by_resource_id(self, resource_id):
+        """
+        click an UI object by resource id.
+        :param resource_id: resource id
+        """
+        self.device(resourceId=resource_id).click()
+
+    def back(self):
+        """
+        press back button.
+        """
+        self.device.press('back')
+
+    def open_app(self, package_name, **kwargs):
+        """
+        open application by package name.
+        :param package_name: package name
+        :param kwargs:
+        :return:
+        """
+        self.device.session(package_name, **kwargs)
+
+    def scroll_to_end(self):
+        """
+        if current screen exist scrollable UI, scroll it to end.
+        """
+        self.device(scrollable=True).scroll.toEnd()
+
+
+def create_action(device):
+    """
+    return an action object for device.
+    :param device: Android or IOS device
+    :return: AndroidAction or IOSAction
+    """
+    return AndroidAction(device)
