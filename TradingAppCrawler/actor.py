49188a4fe7b029cc82adc16a8892648380d9608d
Wen <l1048445160@live.com>
2020-05-09 15:46:00 +0800

    fix(TradingAppCrawler/actor.py): abstract class Actor.

--- TradingAppCrawler/actor.py
+++ TradingAppCrawler/actor.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod, ABC
 
 
-class Actor:
+class Actor(metaclass=ABCMeta):
     """
-    Base Actor.
+    An abstract class for actor.
+    Perform basic operations on the device.
     """
     def __init__(self, device):
         self.device = device
 
-    def open_app(self, package_name):
-        pass
-
-    def refresh(self):
-        pass
-
+    @abstractmethod
     def back(self):
         pass
 
-    def click(self):
-        pass
-
+    @abstractmethod
     def click_by_resource_id(self, resource_id):
         pass
 
+    @abstractmethod
+    def scroll_to_end(self):
+        pass
 
-class AndroidActor(Actor):
+    @abstractmethod
+    def scroll_to_top(self):
+        pass
+
+    @abstractmethod
+    def open_app(self, package_name):
+        pass
+
+
+class AndroidActor(Actor, ABC):
     """
     An actor for Android, interactive with Android device
     """
@@ -57,6 +64,12 @@ class AndroidActor(Actor):
         """
         self.device(scrollable=True).scroll.toEnd()
 
+    def scroll_to_top(self):
+        """
+        if current screen exist scrollable UI, scroll it to top.
+        """
+        self.device(scrollable=True).scroll.toBeginning()
+
 
 def create_actor(device):
     """
@@ -65,3 +78,7 @@ def create_actor(device):
     :return: AndroidActor or IOSActor
     """
     return AndroidActor(device)
+
+
+if __name__ == '__main__':
+    a = AndroidActor(None)
