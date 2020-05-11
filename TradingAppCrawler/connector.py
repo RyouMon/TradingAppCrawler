7656f991eeb3c6ac57f61de1b00da422d0b73f3d
Wen <l1048445160@live.com>
2020-05-11 11:34:52 +0800

    fix(TradingAppCrawler/connector.py): fix connect.

--- TradingAppCrawler/connector.py
+++ TradingAppCrawler/connector.py
@@ -4,15 +4,11 @@ import uiautomator2
 import os
 
 
-def connect(addr, platform='android'):
+def connect(addr):
     """
     connect device, return Device object.
     :param addr: address for device
-    :param platform: device platform, 'android' or 'ios'
     :return: Device
     """
-    if platform == 'android':
-        os.system('adb connect %s' % addr)
-        return uiautomator2.connect(addr=addr)
-    elif platform == 'ios':
-        pass
+    os.system('adb connect %s' % addr)
+    return uiautomator2.connect(addr=addr)
